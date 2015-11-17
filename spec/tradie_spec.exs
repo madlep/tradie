e8dd6e0b80c92bce51f6e5cdeeed9011739ee4f8
Julian Doherty (madlep) <madlep@madlep.com>
2015-11-17 22:40:09 +1100

    clean up specs to use proper let/shared/before setup

--- spec/tradie_spec.exs
+++ spec/tradie_spec.exs
@@ -1,28 +1,33 @@
 defmodule TradieSpec do
   use ESpec
 
-  defp stub_task(result, sleep_time \\ 0)
-  defp stub_task(result, 0) do
+  def stub_task(result, sleep_time \\ 0)
+  def stub_task(result, 0) do
     fn() ->
       result
     end
   end
 
-  defp stub_task(result, sleep_time) do
+  def stub_task(result, sleep_time) do
     fn() ->
       :timer.sleep(sleep_time)
       result
     end
   end
 
-  it "runs tasks specified" do
-    tradie_tasks = Tradie.async([
-      stub_task("foo"),
-      stub_task("bar", 1)
-    ])
+  describe "async/await" do
+    context "when all tasks complete" do
+      let :tradie_tasks, do: Tradie.async([
+        TradieSpec.stub_task("foo"),
+        TradieSpec.stub_task("bar", 1)
+      ])
+      before do: {:shared, results: Tradie.await(tradie_tasks) }
+      let :expected_results, do: [{:ok, "foo"}, {:ok, "bar"}]
 
+      it "runs tasks specified" do
+        expect(shared.results) |> to eq(expected_results)
+      end
+    end
 
-    results = Tradie.await(tradie_tasks)
-    expect(results) |> to eq([{:ok, "foo"}, {:ok, "bar"}])
   end
 end
