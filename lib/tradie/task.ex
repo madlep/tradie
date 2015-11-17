649430c74012182bc81674dd947ae7e5bdfd47a4
Julian Doherty (madlep) <madlep@madlep.com>
2015-11-17 21:28:06 +1100

    use structs internally

--- lib/tradie/task.ex
+++ lib/tradie/task.ex
@@ -1,24 +1,35 @@
 defmodule Tradie.Task do
-  def create_task(work_ref, supervisor, fun) do
-    caller = self
-    task_ref = make_ref
-    {:ok, pid} = Task.Supervisor.start_child(
-      supervisor,
-      __MODULE__, :run_task, [work_ref, task_ref, caller, fun]
-    )
-    task_ref
+  defstruct [:work_ref, :task_ref, :caller, :fun]
+
+  defmodule Result do
+    defstruct [:work_ref, :task_ref, :result]
   end
 
-  def run_task(work_ref, task_ref, caller, fun) do
-    result = {work_ref, task_ref, do_run_task(fun)}
+  def create_task(work_ref, supervisor, fun) do
+    task = %Tradie.Task{
+      work_ref: work_ref,
+      task_ref: make_ref,
+      caller: self,
+      fun: fun
+    }
+
+    {:ok, pid} = Task.Supervisor.start_child(
+      supervisor,
+      __MODULE__, :run_task, [task]
+    )
+    task
+  end
+
+  def run_task(%Tradie.Task{work_ref: work_ref, task_ref: task_ref, caller: caller, fun: fun}) do
+    result = %Result{work_ref: work_ref, task_ref: task_ref, result: do_run_task(fun)}
     send(caller, result)
   end
 
   defp do_run_task(fun) when is_function(fun), do: fun.()
 
-  def receive_result(work_ref, task_ref) do
+  def receive_result(work_ref, %Tradie.Task{work_ref: work_ref, task_ref: task_ref}) do
     receive do
-      {^work_ref, ^task_ref, result} -> {:ok, result}
+      %Result{work_ref: ^work_ref, task_ref: ^task_ref, result: result} -> {:ok, result}
     end
   end
 end
