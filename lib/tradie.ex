4bdd5a1cddb23086d4658c0626970074c25f1ecd
Julian Doherty (madlep) <madlep@madlep.com>
2015-11-17 22:17:53 +1100

    pipeline all the things

--- lib/tradie.ex
+++ lib/tradie.ex
@@ -5,17 +5,17 @@ defmodule Tradie do
 
   def async(funs) do
     {:ok, supervisor} = Task.Supervisor.start_link(restart: :transient)
-    tradie = %Tradie{work_ref: make_ref, supervisor: supervisor}
-    start_tasks(tradie, funs)
+    %Tradie{work_ref: make_ref, supervisor: supervisor}
+    |> start_tasks(funs)
   end
 
   defp start_tasks(tradie = %Tradie{work_ref: work_ref, supervisor: supervisor}, funs) do
-    %Tradie{tradie | tasks: Enum.map(
-      funs, &Tradie.Task.create_task(work_ref, supervisor, &1)
-    )}
+    %Tradie{tradie |
+      tasks: funs |> Enum.map(&Tradie.Task.create_task(work_ref, supervisor, &1))
+    }
   end
 
   def await(%Tradie{tasks: tasks}, _timeout \\ 5000) do
-    Enum.map(tasks, &Tradie.Task.receive_result(&1))
+    tasks |> Enum.map(&Tradie.Task.receive_result(&1))
   end
 end
