6f66dbf6f61550eed64da4437a8e1d251d05a072
Julian Doherty (madlep) <madlep@madlep.com>
2015-11-18 23:07:56 +1100

    set up hex.pm metadata

--- mix.exs
+++ mix.exs
@@ -8,7 +8,9 @@ defmodule Tradie.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps,
-     preferred_cli_env: [espec: :test]
+     preferred_cli_env: [espec: :test],
+     description: description,
+     package: package
    ]
   end
 
@@ -31,6 +33,22 @@ defmodule Tradie.Mixfile do
   defp deps do
     [
       {:espec, "~> 0.8.5", only: :test}
+    ]
+  end
+
+  defp description do
+    """
+    Execute multiple tasks in parallel, allowing retry for each task, and a global timeout. Based loosely on http://theerlangelist.com/article/beyond_taskasync.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Julian Doherty"],
+      licenses: ["MIT License"],
+      links: %{
+        "Github" => "https://github.com/madlep/tradie"
+      }
     ]
   end
 end
